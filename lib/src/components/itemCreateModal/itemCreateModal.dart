db559b5a0760c69f40763df77e0695c91909cfcc
SpencerCornish <spencerjcornish@gmail.com>
2017-10-09 16:45:20 -0600

    Initial testing of animations without Angular4

--- lib/src/components/itemCreateModal/itemCreateModal.dart
+++ lib/src/components/itemCreateModal/itemCreateModal.dart
@@ -1,9 +1,12 @@
+import 'dart:html';
+import 'dart:async';
 import 'package:angular2/core.dart';
 import 'package:built_redux/built_redux.dart';
 
 import '../../state/app.dart';
 import '../../models/category.dart';
 import '../../store.dart';
+import '../../constants.dart';
 import '../../middleware/creationMiddleware.dart';
 
 @Component(
@@ -13,7 +16,8 @@ import '../../middleware/creationMiddleware.dart';
 class ItemCreateModalComponent {
   final Store<App, AppBuilder, AppActions> _store;
 
-  ItemCreateModalComponent(StoreService storeService) : _store = storeService.store;
+  ItemCreateModalComponent(StoreService storeService)
+      : _store = storeService.store;
 
   Category get category => _store.state.categories.current;
 
@@ -32,8 +36,16 @@ class ItemCreateModalComponent {
 
   void resetItemForm() {
     description = "";
-    _store.actions.hideModal(null);
+    hideModal();
   }
 
-  void hideModal() => _store.actions.hideModal(null);
+  hideModal() async {
+    Element modalCard = document.querySelector('.modal-card');
+    Element modalBackground = document.querySelector('.modal-background');
+    modalCard.className = "modal-card animated zoomOut fast";
+    modalBackground.className = "modal-background animated fadeOut fast";
+
+    new Future.delayed(
+        new Duration(milliseconds: 300), () => _store.actions.hideModal(null));
+  }
 }
