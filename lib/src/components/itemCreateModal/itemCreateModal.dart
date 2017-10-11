b3cf6ec484c65b87941032b36a381b5060912f16
SpencerCornish <spencerjcornish@gmail.com>
2017-10-10 19:16:21 -0600

    revert animation

--- lib/src/components/itemCreateModal/itemCreateModal.dart
+++ lib/src/components/itemCreateModal/itemCreateModal.dart
@@ -6,7 +6,6 @@ import 'package:built_redux/built_redux.dart';
 import '../../state/app.dart';
 import '../../models/category.dart';
 import '../../store.dart';
-import '../../constants.dart';
 import '../../middleware/creationMiddleware.dart';
 
 @Component(
@@ -36,16 +35,8 @@ class ItemCreateModalComponent {
 
   void resetItemForm() {
     description = "";
-    hideModal();
+    _store.actions.hideModal(null);
   }
 
-  hideModal() async {
-    Element modalCard = document.querySelector('.modal-card');
-    Element modalBackground = document.querySelector('.modal-background');
-    modalCard.className = "modal-card animated zoomOut fast";
-    modalBackground.className = "modal-background animated fadeOut fast";
-
-    new Future.delayed(
-        new Duration(milliseconds: 300), () => _store.actions.hideModal(null));
-  }
+  void hideModal() => _store.actions.hideModal(null);
 }
